6a3253f4be637bc83300872b44aa753ffc01d84d
MD. Saiful Islam Feroz <saif012@gmail.com>
2023-08-16 23:06:25 +0600

    feat(Test): add bootstrap

---
+++ Tests/bootstrap.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+ * This file is part of the XiideaEasyConfigBundle package.
+ *
+ * (c) Xiidea <http://www.xiidea.net>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+date_default_timezone_set('Asia/Dhaka');
+
+$vendor = realpath(__DIR__.'/../vendor');
+
+$loader = require $vendor.'/autoload.php';
